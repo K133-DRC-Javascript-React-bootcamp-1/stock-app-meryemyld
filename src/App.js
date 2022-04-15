1756af8164fa75c698b4b3cd4e41d1661452c62b
Meryem Yıldız <97787178+meryemyld@users.noreply.github.com>
2022-04-15 22:59:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,9 +15,6 @@ import { Col, Row, Container } from 'reactstrap';
 
 function App() {
 
-  let a={ id:"1", name:"meryem", surname:"yildiz", age:"24", mail:"meryemyld"}
-  let b={ id:"2", name:"emre", surname:"tek", age:"28", mail:"emretek"}
-
   return (
     <div className="App">
       <Navbar1/>
